eef5c9cf26a9b6b087a880a6d6ab0be8e53c94b3
hossamdev <hossamtoubali@gmail.com>
2024-02-07 16:29:35 +0100

    Initialize project and install reactjs and tailwindcss

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className=' text-3xl  font-bold bg-slate-500 text-cyan-500 text-center'>Hello Auth</h1>
+    </>
+  )
+}
+
+export default App
